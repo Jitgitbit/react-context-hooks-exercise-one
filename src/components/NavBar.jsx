6fbaa27bc856d61a7978bf8de910f845dca5bae0
Jitgitbit <rowowolf@gmail.com>
2020-03-23 11:50:26 +0100

    another way: using the Consumer(works for fn comp)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,18 +4,22 @@ import {ThemeContext} from '../contexts/ThemeContext'
 export default class NavBar extends Component {
   static contextType = ThemeContext;
   render() {
-    console.log(this.context);
-    const {isLightTheme, light, dark} = this.context;
-    const theme = isLightTheme ? light : dark ; 
+    // console.log(this.context);
     return (
-      <nav style={{ background: theme.ui, color: theme.textColor }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>{(context) => {
+        const {isLightTheme, light, dark} = context;
+        const theme = isLightTheme ? light : dark ; 
+        return(
+          <nav style={{ background: theme.ui, color: theme.textColor }}>
+          <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        );
+      }}</ThemeContext.Consumer>
     )
   }
 }

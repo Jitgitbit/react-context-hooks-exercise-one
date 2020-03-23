7a31db23e406c57c8d96a2b9537a3af9cd9ffd14
Jitgitbit <rowowolf@gmail.com>
2020-03-23 11:41:05 +0100

    coloring passed to bookList. PRETTY !!!

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
+import {ThemeContext} from '../contexts/ThemeContext'
 
 export default class BookList extends Component {
+  static contextType = ThemeContext;
   render() {
+    const {isLightTheme, light, dark} = this.context;
+    const theme = isLightTheme ? light : dark ; 
     return (
-      <div className='book-list'>
+      <div className='book-list' style={{ color: theme.colorText, background: theme.bg }}>
         <ul>
-          <li>The way of the world</li>
-          <li>Black pigeon speaks</li>
-          <li>Veritas</li>
-          <li>Gravitas</li>
+          <li style={{ background: theme.ui }}>The way of the world</li>
+          <li style={{ background: theme.ui }}>Black pigeon speaks</li>
+          <li style={{ background: theme.ui }}>Veritas</li>
+          <li style={{ background: theme.ui }}>Gravitas</li>
         </ul>
       </div>
     )

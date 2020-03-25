e4645c67d59b5e052d326dae4ec3914d57dafaf9
Jitgitbit <rowowolf@gmail.com>
2020-03-25 18:52:50 +0100

    HOOKS !     useContext

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,20 +1,34 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import {ThemeContext} from '../contexts/ThemeContext'
 
-export default class BookList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const {isLightTheme, light, dark} = this.context;
-    const theme = isLightTheme ? light : dark ; 
-    return (
-      <div className='book-list' style={{ color: theme.colorText, background: theme.bg }}>
-        <ul>
-          <li style={{ background: theme.ui }}>The way of the world</li>
-          <li style={{ background: theme.ui }}>Black pigeon speaks</li>
-          <li style={{ background: theme.ui }}>Veritas</li>
-          <li style={{ background: theme.ui }}>Gravitas</li>
-        </ul>
-      </div>
-    )
-  }
+// export default class BookList extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const {isLightTheme, light, dark} = this.context;
+//     const theme = isLightTheme ? light : dark ; 
+//     return (
+//       <div className='book-list' style={{ color: theme.colorText, background: theme.bg }}>
+//         <ul>
+//           <li style={{ background: theme.ui }}>The way of the world</li>
+//           <li style={{ background: theme.ui }}>Black pigeon speaks</li>
+//           <li style={{ background: theme.ui }}>Veritas</li>
+//           <li style={{ background: theme.ui }}>Gravitas</li>
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+export default function BookList() {
+  const {isLightTheme, light, dark} = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark ; 
+  return (
+    <div className='book-list' style={{ color: theme.colorText, background: theme.bg }}>
+      <ul>
+        <li style={{ background: theme.ui }}>The way of the world</li>
+        <li style={{ background: theme.ui }}>Black pigeon speaks</li>
+        <li style={{ background: theme.ui }}>Veritas</li>
+        <li style={{ background: theme.ui }}>Gravitas</li>
+      </ul>
+    </div>
+  )
 }

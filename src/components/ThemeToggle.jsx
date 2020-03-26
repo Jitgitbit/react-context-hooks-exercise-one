c4e542d2dd8e54d8cd826977495e449bf6e921dc
Jitgitbit <rowowolf@gmail.com>
2020-03-26 14:35:29 +0100

    all components converted to fn cmp and with hooks

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import {ThemeContext} from '../contexts/ThemeContext'
 
-export default class ThemeToggle extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const {toggleTheme} = this.context;
-    return (
-      <button onClick={toggleTheme}>Toggle dark/light</button>
-    )
-  }
+// export default class ThemeToggle extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const {toggleTheme} = this.context;
+//     return (
+//       <button onClick={toggleTheme}>Toggle dark/light</button>
+//     )
+//   }
+// }
+
+export default function ThemeToggle() {
+  const {toggleTheme} = useContext(ThemeContext);
+  return (
+    <button onClick={toggleTheme}>Toggle dark/light</button>
+  )
 }

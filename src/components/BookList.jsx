53630f8132c6b29c3dddfea04068027b472f7640
Jitgitbit <rowowolf@gmail.com>
2020-03-26 14:55:22 +0100

    BookContext in use, no more HC

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import {ThemeContext} from '../contexts/ThemeContext'
+import { BookContext } from '../contexts/BookContext';
 
 // export default class BookList extends Component {
 //   static contextType = ThemeContext;
@@ -20,14 +21,20 @@ import {ThemeContext} from '../contexts/ThemeContext'
 // }
 export default function BookList() {
   const {isLightTheme, light, dark} = useContext(ThemeContext);
+  const {books} = useContext(BookContext);
   const theme = isLightTheme ? light : dark ; 
   return (
     <div className='book-list' style={{ color: theme.colorText, background: theme.bg }}>
       <ul>
-        <li style={{ background: theme.ui }}>The way of the world</li>
+        {books.map(book => {
+          return(
+            <li style={{ background: theme.ui }} key={book.id}>{book.title}</li>
+          )
+        })}
+        {/* <li style={{ background: theme.ui }}>The way of the world</li>
         <li style={{ background: theme.ui }}>Black pigeon speaks</li>
         <li style={{ background: theme.ui }}>Veritas</li>
-        <li style={{ background: theme.ui }}>Gravitas</li>
+        <li style={{ background: theme.ui }}>Gravitas</li> */}
       </ul>
     </div>
   )
